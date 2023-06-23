c3c0c9fb6d3ed2255a565320ea0b23ab4d9330f7
ShalaevPavel <lohgewt123@yandex.by>
2023-06-23 16:11:20 +0300

    it turned out that client .query method is related to notification recieving

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,79 @@ import { Client } from 'pg';
 const pgClient = new Client({
     user: 'user1',
     host: 'localhost',
-    database: 'chat_ts',
+    database: 'tz_2',
+    // database: 'chat_ts',
     password: '12345678',
     port: 5432, // Порт PostgreSQL
 });
 
-pgClient.connect();
+
 
 // Создание экземпляра бота Telegraf
 const bot = new Telegraf('5973636409:AAH46_rWxHYdZWtbReockDQma0hqDf5Yv1g');
 
+
+
 // Обработчик команды /start
-bot.start((ctx: Context) => {
-    ctx.reply('Привет! Я бот отслеживания покупок 69420.');
-});
+// bot.start((ctx: Context) => {
+//     ctx.reply('Питер - интернетная помойка');
+// });
 
 // Обработчик покупки
-bot.command('purchase', async (ctx: Context) => {
+// bot.command('purchase', async (ctx: Context) => {
+//     try {
+//         // Получение информации о покупке из базы данных
+//         const result = await pgClient.query('SELECT * FROM purchases');
+//         const purchases = result.rows;
+//
+//         // Отправка информации о покупке в телеграм-бота
+//         for (const purchase of purchases) {
+//             ctx.reply(`Новая покупка: ${purchase.name} bought for ${purchase.price}`);
+//
+//         }
+//     } catch (error) {
+//         console.error('Ошибка при получении информации о покупке:', error);
+//     }
+// });
+async function handlePurchaseMessage(message: any) {
+    const chatId = '626266495'; // Замените на ID админского чата
+
     try {
-        // Получение информации о покупке из базы данных
-        const result = await pgClient.query('SELECT * FROM purchases');
-        const purchases = result.rows;
-
-        // Отправка информации о покупке в телеграм-бота
-        for (const purchase of purchases) {
-            ctx.reply(`Новая покупка: ${purchase.name}`);
-
-        }
+        // const telegramMessage = JSON.parse(message.payload);
+        const telegramMessage: string = message.leangth;
+        await bot.telegram.sendMessage(chatId, telegramMessage);
+        await bot.telegram.sendMessage(chatId, "13224224");
     } catch (error) {
-        console.error('Ошибка при получении информации о покупке:', error);
+        console.error(error);
     }
+}
+
+// pgClient.on('notification', (notification) => {
+//     console.log('Received notification:', notification);
+//     handlePurchaseMessage(notification);
+//     // const purchaseName = notification.payload;
+//     // console.log(purchaseName);
+//     // bot.telegram.sendMessage('626266495', `Новая покупка: ${purchaseName}`);
+// });
+
+pgClient.connect((err) => {
+    if (err) {
+        console.error('connection error', err.stack);
+        process.exit(-1);
+    }
+
+    console.log('connected');
+
+    pgClient.query('LISTEN purchases_queue');
+
+    pgClient.on('notification', (notification) => {
+        console.log('Received notification:', notification);
+        console.log(notification.payload);
+        bot.telegram.sendMessage('626266495', notification.toString());
+        handlePurchaseMessage(notification);
+    });
 });
+
 
 // Запуск бота
 bot.launch().then(() => {
@@ -43,3 +85,16 @@ bot.launch().then(() => {
 }).catch((error) => {
     console.error('Ошибка при запуске бота:', error);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
